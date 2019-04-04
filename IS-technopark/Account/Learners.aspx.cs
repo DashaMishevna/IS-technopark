1301c2941001a1ac009cee3e81c63710a293409e
DashaMishevna <Dasytka--0912@mail.ru>
2019-04-04 23:57:50 +0400

    DropL

--- IS-technopark/Account/Learners.aspx.cs
+++ IS-technopark/Account/Learners.aspx.cs
@@ -16,8 +16,14 @@ namespace IS_technopark.Account
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            f(!IsPostBack)
+            {
+                GetDropList();
+                DropDownList2.Items.Insert(0, new ListItem("-Выберете проект-"));
+            }
         }
+
+        
 
         protected void Button1_Click(object sender, EventArgs e)
         {
@@ -32,14 +38,34 @@ namespace IS_technopark.Account
 
         protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (DropDownList1.SelectedValue.ToString() != "0")
+            {
+                oraConnection.Open();
+                string s1 = "Select Title from DIR_PROJECTS WHERE id_Laboratory = ( Select id_Laboratories from DIR_LABORATORIES where LABORATORY = '" + DropDownList1.SelectedValue.ToString() + "')";
+                OracleDataAdapter oraAdap = new OracleDataAdapter(s1, oraConnection);
+                oraAdap.Fill(ds);
+                DropDownList2.DataSource = ds;
+                DropDownList2.DataTextField = "Title";
+                DropDownList2.DataBind();
+                DropDownList2.Items.Insert(0, new ListItem("-Выберете проект-"));
+                DropDownList2.SelectedIndex = 0;
+            }
+        }
+
+        private void GetDropList()
+        {
             oraConnection.Open();
-            string s1 = "Select Title from DIR_PROJECTS WHERE id_Laboratory = ( Select id_Laboratories from DIR_LABORATORIES where LABORATORY = '" + DropDownList1.SelectedValue.ToString() + "')";
+            string s1 = "Select Laboratory from DIR_LABORATORIES";
             OracleDataAdapter oraAdap = new OracleDataAdapter(s1, oraConnection);
             oraAdap.Fill(ds);
-            DropDownList2.DataSource = ds;
-            DropDownList2.DataTextField = "Title";
-            DropDownList2.DataBind();
-            DropDownList2.SelectedIndex = 0;
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                DropDownList1.DataSource = ds;
+                DropDownList1.DataTextField = "Laboratory";
+                DropDownList1.DataBind();
+                DropDownList1.Items.Insert(0, new ListItem("-Выберете направление-"));
+                DropDownList1.SelectedIndex = 0;
+            }
         }
     }
 }
