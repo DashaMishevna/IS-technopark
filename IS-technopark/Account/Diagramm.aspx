18bbe7bf5f1040e7a6f8e0b5f15145f794e64ad9
DashaMishevna <Dasytka--0912@mail.ru>
2019-04-18 21:11:27 +0400

    Рабочая диаграмма - Пирог

--- IS-technopark/Account/Diagramm.aspx
+++ IS-technopark/Account/Diagramm.aspx
@@ -3,13 +3,19 @@
 <%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
 
 <asp:Content  ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <asp:Chart ID="Chart1" runat="server">
+    <asp:Chart ID="Chart1" runat="server" Height="550px" Width="550px">
         <Series>
-            <asp:Series Name="Series1"></asp:Series>
+            <asp:Series Name="Series1" Legend="Legend1"></asp:Series>
         </Series>
         <ChartAreas>
-            <asp:ChartArea Name="ChartArea1"></asp:ChartArea>
+            <asp:ChartArea Name="ChartArea1" ShadowColor="LightSkyBlue"></asp:ChartArea>
         </ChartAreas>
+        <Legends>
+            <asp:Legend Name="Legend1">
+            </asp:Legend>
+        </Legends>
     </asp:Chart>
 
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
 </asp:Content>
--- IS-technopark/Account/Diagramm.aspx.cs
+++ IS-technopark/Account/Diagramm.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Web;
@@ -11,23 +13,69 @@ namespace IS_technopark.Account
 {
     public partial class Diagramm : System.Web.UI.Page
     {
+        OracleDataAdapter oraAdap = new OracleDataAdapter();
+        OracleConnection oraConnection = new OracleConnection("Data Source =127.0.0.1:1521/xe; User ID =Technopark;  password = DIP1937;");
+        DataTable table = new DataTable();
+        DataSet ds = new DataSet();
+        List<string> count = new List<string>();
+        List<string> count1 = new List<string>();
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            Chart1.BorderlineDashStyle = ChartDashStyle.Solid;
-            Chart1.BorderlineColor = Color.Gray;
-            Chart1.BorderSkin.SkinStyle = BorderSkinStyle.Emboss;
+            oraConnection.Open();
+            //Chart1.BorderlineDashStyle = ChartDashStyle.Solid;
+            //Chart1.BorderlineColor = Color.Gray;
+            //Chart1.BorderSkin.SkinStyle = BorderSkinStyle.Emboss;
 
-            Chart1.Titles.Add("ASP.NET Chart");
+            Chart1.Titles.Add("Диаграмма по направлениям");
             Chart1.Titles[0].Font = new Font("Utopia", 16);
 
-            Chart1.Series.Add(new Series("ColumnSeries")
+            //Chart1.Series.Add(new Series("ColumnSeries")
+            //{
+            //    ChartType = SeriesChartType.Pie
+            //});
+
+            oraAdap.SelectCommand = new OracleCommand();
+            oraAdap.SelectCommand.CommandText = "select DIR_LABORATORIES.LABORATORY from QUEUE, DIR_LABORATORIES WHERE QUEUE.ID_LABORATORIES=DIR_LABORATORIES.ID_LABORATORIES group by DIR_LABORATORIES.LABORATORY";
+            oraAdap.SelectCommand.Connection = oraConnection;
+            OracleDataReader oraReader = oraAdap.SelectCommand.ExecuteReader();
+            while (oraReader.Read())
             {
-                ChartType = SeriesChartType.Pie
-            });
+                object[] values = new object[oraReader.FieldCount];
+                oraReader.GetValues(values);
+                count.Add(values[0].ToString());
+            }
 
-            //select DIR_LABORATORIES.LABORATORY from QUEUE, DIR_LABORATORIES WHERE QUEUE.ID_LABORATORIES=DIR_LABORATORIES.ID_LABORATORIES group by DIR_LABORATORIES.LABORATORY;
+            oraAdap.SelectCommand = new OracleCommand();
+            oraAdap.SelectCommand.CommandText = "select Count(ID_queue) from QUEUE, DIR_LABORATORIES WHERE QUEUE.ID_LABORATORIES = DIR_LABORATORIES.ID_LABORATORIES group by DIR_LABORATORIES.LABORATORY";
+            oraAdap.SelectCommand.Connection = oraConnection;
+            OracleDataReader oraReader1 = oraAdap.SelectCommand.ExecuteReader();
+            while (oraReader1.Read())
+            {
+                object[] values = new object[oraReader1.FieldCount];
+                oraReader1.GetValues(values);
+                count1.Add(values[0].ToString());
+            }
 
-            //select Count(ID_queue)from QUEUE, DIR_LABORATORIES WHERE QUEUE.ID_LABORATORIES = DIR_LABORATORIES.ID_LABORATORIES group by DIR_LABORATORIES.LABORATORY;
+            string[] x = new string[count.Count];
+            int[] y = new int [count1.Count];
+
+            for (int i = 0; i < count.Count; i++)
+            {
+                x[i] = count[i].ToString();
+            }
+
+            for (int i = 0; i < count1.Count; i++)
+            {
+                y[i] = Convert.ToInt32(count1[i]);
+            }
+
+            Chart1.Series[0].Points.DataBindXY(x,y);
+            Chart1.Series[0].ChartType = SeriesChartType.Pie;
+            Chart1.Legends[0].Enabled = true;
+            //GridView1.DataSource = count1;
+            //GridView1.DataBind();
+            oraConnection.Close();
         }
     }
 }
