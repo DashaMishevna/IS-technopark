702a755aff22be955684b36baa0e2c67bb52a4d6
DashaMishevna <Dasytka--0912@mail.ru>
2019-07-03 00:14:44 +0400

    Статистика

--- IS-technopark/Account/Diagramm_Interests.aspx.cs
+++ IS-technopark/Account/Diagramm_Interests.aspx.cs
@@ -34,29 +34,32 @@ namespace IS_technopark.Account
             //GetDropList();
             //Chart1.Titles.Add("Диаграмма по интересам");
             //Chart1.Titles[0].Font = new Font("Utopia", 16);
-
-            oraAdap.SelectCommand = new OracleCommand();
-            oraAdap.SelectCommand.CommandText = "select Count(ID_Learner), class from Learner WHERE INTERESTS Like '%" + DropDownList1.Text + "%' group by Class";
-            oraAdap.SelectCommand.Connection = oraConnection;
-            OracleDataReader oraReader = oraAdap.SelectCommand.ExecuteReader();
-            while (oraReader.Read())
+            if (Class_FIO.Employees_position == "2")
             {
-                object[] values = new object[oraReader.FieldCount];
-                oraReader.GetValues(values);
-                List_x.Add(values[0].ToString());
-                List_y.Add(values[1].ToString());
+
+                oraAdap.SelectCommand = new OracleCommand();
+                oraAdap.SelectCommand.CommandText = "select Count(ID_Learner), class from Learner WHERE INTERESTS Like '%" + DropDownList1.Text + "%' group by Class";
+                oraAdap.SelectCommand.Connection = oraConnection;
+                OracleDataReader oraReader = oraAdap.SelectCommand.ExecuteReader();
+                while (oraReader.Read())
+                {
+                    object[] values = new object[oraReader.FieldCount];
+                    oraReader.GetValues(values);
+                    List_x.Add(values[0].ToString());
+                    List_y.Add(values[1].ToString());
+                }
+
+                string command = SqlDataSource1.SelectCommand;
+                SqlDataSource1.SelectCommand = "select Count(ID_Learner) as Количество, class as Класс from Learner WHERE INTERESTS Like '%" + DropDownList1.Text + "%' group by Class";
+                SqlDataSource1.DataBind();
+                GridView1.DataBind();
+
+                Chart1.Series[0].Name = DropDownList1.Text;
+                Chart1.Series[0].Points.DataBindXY(List_y, List_x);
+                Chart1.Series[0].ChartType = SeriesChartType.Column;
+                Chart1.Legends[0].Enabled = true;
+                oraConnection.Close();
             }
-
-            string command = SqlDataSource1.SelectCommand;
-            SqlDataSource1.SelectCommand = "select Count(ID_Learner) as Количество, class as Класс from Learner WHERE INTERESTS Like '%" + DropDownList1.Text + "%' group by Class";
-            SqlDataSource1.DataBind();
-            GridView1.DataBind();
-
-            Chart1.Series[0].Name = DropDownList1.Text;
-            Chart1.Series[0].Points.DataBindXY(List_y, List_x);
-            Chart1.Series[0].ChartType = SeriesChartType.Column;
-            Chart1.Legends[0].Enabled = true;
-            oraConnection.Close();
         }
 
         private void GetDropList()
